1cbb27366486f796b82e3af0f403ec5664761057
albertmanuel <albertmanuels10@gmail.com>
2025-11-27 20:08:03 +0700

    feat: slicing explore screen

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -1,22 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { CategoryType } from "@/types/type";
+import { Stack } from "expo-router";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { Colors } from "@/constants/Colors";
 
-type Props = {}
+type Props = {};
 
 const ExploreScreen = (props: Props) => {
-  return (
-    <View style={styles.container}>
-      <Text>Explore Screen</Text>
-    </View>
-  )
-}
+  const [categories, setCategories] = React.useState<CategoryType[]>([]);
 
-export default ExploreScreen
+  const getCategories = async () => {
+    const URL = `http://localhost:8000/categories`;
+    const response = await axios.get(URL);
+    setCategories(response.data);
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  const headerHeight = useHeaderHeight();
+
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: true, headerTransparent: true }} />
+      <View style={[styles.container, { marginTop: headerHeight }]}>
+        <FlatList
+          data={categories}
+          keyExtractor={(item) => item.id.toString()}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <View style={styles.itemWrapper}>
+              <Text style={styles.itemText}>{item.name}</Text>
+              <Image
+                source={{ uri: item.image }}
+                style={{ width: 100, height: 100, borderRadius: 10 }}
+              />
+            </View>
+          )}
+        />
+      </View>
+    </>
+  );
+};
+
+export default ExploreScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    paddingHorizontal: 20,
+  },
+  itemWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: Colors.extraLightGray,
+    marginBottom: 20,
+  },
+  itemText: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: Colors.black,
+  },
+});
